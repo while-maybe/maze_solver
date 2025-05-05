96bb54c3cc1592421f22116763eb7f25046d1750
while-maybe <178353419+while-maybe@users.noreply.github.com>
2025-05-05 21:18:25 +0100

    feat: implement entrance finding logic in maze solver

--- internal/solver/solver.go
+++ internal/solver/solver.go
@@ -1,6 +1,14 @@
 package solver
 
-import "image"
+import (
+	"fmt"
+	"image"
+	"log"
+)
+
+const (
+	ErrNoEntrance = Error("entrance position not found")
+)
 
 // Solver is capable of finding the path from the entrance to the treasure.
 // The maze has to be an RGB image
@@ -24,5 +32,23 @@ func New(imagePath string) (*Solver, error) {
 
 // Solve finds the path from the entrance to the treasure.
 func (s *Solver) Solve() error {
+	entrance, err := s.findEntrance()
+	if err != nil {
+		return fmt.Errorf("solver didn't find an entrance: %w", err)
+	}
+
+	log.Printf("starting at pos: %v", entrance)
 	return nil
+}
+
+// findEntrance returns the position of the maze entrance on the image.
+func (s *Solver) findEntrance() (image.Point, error) {
+	for row := s.maze.Bounds().Min.Y; row < s.maze.Bounds().Max.Y; row++ {
+		for col := s.maze.Bounds().Min.X; col < s.maze.Bounds().Max.X; col++ {
+			if s.maze.RGBAAt(col, row) == s.palette.entrance {
+				return image.Point{col, row}, nil
+			}
+		}
+	}
+	return image.Point{}, ErrNoEntrance
 }
